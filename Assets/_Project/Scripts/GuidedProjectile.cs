72e6706bb07b8b03f9755be7f3b75d99ab36bcd6
David Abruzzo <davidabruzzogames@gmail.com>
2025-08-02 18:01:33 +0200

    smoke animation added to guided projectile

--- Assets/_Project/Scripts/GuidedProjectile.cs
+++ Assets/_Project/Scripts/GuidedProjectile.cs
@@ -10,6 +10,8 @@ public class GuidedProjectile : MonoBehaviour
     private Rigidbody2D rb;
     private float lifetime;
     private Vector3 velocity;
+    public GameObject animationPrefab;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -82,4 +84,13 @@ public class GuidedProjectile : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+ 
+        void OnDestroy()
+        {
+            GameObject fx = Instantiate(animationPrefab, transform.position, Quaternion.identity);
+            fx.transform.SetParent(null); // détaché du parent
+        }
+
+   
 }
---
+++ Assets/_Project/Scripts/SmokeAnimation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SmokeAnimation : MonoBehaviour
+{
+  
+    
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    public void destroy()
+    {
+        Destroy(gameObject);
+    }
+
+}
